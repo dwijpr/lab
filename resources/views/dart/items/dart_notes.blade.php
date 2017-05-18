9b80c103ed1ba5ec3919e36caa78d82e8b93d24e
dwijpr <dwijpr@gmail.com>
2017-05-18 19:28:14 +0700

    update

--- resources/views/dart/items/dart_notes.blade.php
+++ resources/views/dart/items/dart_notes.blade.php
@@ -3,7 +3,11 @@
 @section('__content')
 <ul>
     <li>
+        Table Resource Controller Laravel unresponsive
+    </li>
+    <li>
         MacBook Laravel Responsive Design
+        <i class="fa fa-check text-success"></i>
     </li>
 </ul>
 @endsection
---
+++ resources/views/dart/items/lab_notes.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.dart')
+
+@section('__content')
+<ul>
+    <li>
+        Sync favicon and images (main, side)
+    </li>
+</ul>
+@endsection
