fbc3ad874a308b2e476c4f616d95f7de6bd39e39
dwijpr <dwijpr@gmail.com>
2017-05-27 13:21:13 +0700

    refactor

--- resources/views/terminal/script.blade.js
+++ resources/views/terminal/script.blade.js
@@ -1,31 +1,41 @@
 var cursor = 0;
 var command = "";
 var terminal = $("#terminal");
+var cursorTemplate = "<i style='color: white;' class='fa fa-i-cursor'></i>";
 
 String.prototype.insert = function (index, string) {
-    if (index > 0)
-        return this.substring(0, index) + string + this.substring(index, this.length);
-    else
+    if (index > 0) {
+        return this.substring(0, index)
+            + string + this.substring(index, this.length);
+    }
+    else {
         return string + this;
+    }
 };
 
-function setCharAt(str, index, chr) {
-    if (index > str.length - 1) return str;
-    return str.substr(0, index) + chr + str.substr(index + 1);
-}
+String.prototype.removeChar = function (index) {
+    if (index > this.length - 1) {
+        return this;
+    }
+    return this.substr(0, index) + this.substr(index + 1);
+};
 
 function draw(callback) {
     var html = "";
     var i = 0;
     while (i < command.length) {
-        html = html + command[i];
+        var char = command[i];
+        if (char == ' ') {
+            char = '&nbsp;';
+        }
+        html = html + char;
         if (i + 1 == cursor) {
-            html = html + "<i style='color: white;' class='fa fa-i-cursor'></i>";
+            html = html + cursorTemplate;
         }
         i++;
     }
     if (cursor == 0) {
-        html = "<i style='color: white;' class='fa fa-i-cursor'></i>" + html;
+        html = cursorTemplate + html;
     }
     terminal.find(".command").html(html);
 }
@@ -48,10 +58,10 @@ $(document).keydown(function(e) {
             break;
         case 'Backspace':
             cursor = cursor - 1;
+            command = command.removeChar(cursor);
             if (cursor < 0) {
                 cursor = 0;
             }
-            command = setCharAt(command, cursor, '');
             draw();
             break;
     }
