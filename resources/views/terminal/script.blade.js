5ea4db29062536035aa805fd197cb77254602c53
dwijpr <dwijpr@gmail.com>
2017-05-28 20:55:00 +0700

    output

--- resources/views/terminal/script.blade.js
+++ resources/views/terminal/script.blade.js
@@ -21,6 +21,24 @@ $.fn.selectRange = function(start, end) {
 var terminal = $("#terminal");
 var commandTemplate = $(".command");
 
+function output(value) {
+    var div = $("<div></div>");
+    div.html(value);
+    terminal.append(div);
+}
+
+function executeCommand(command) {
+    if (command == 'help') {
+        output("GNU bash, version 4.3.42(5)-release (x86_64-pc-msys)");
+        output("These shell commands are defined internally.  Type `help' to see this list.");
+        output("Type `help name' to find out more about the function `name'.");
+        output("Use `info bash' to find out more about the shell in general.");
+        output("Use `man -k' or `info' to find out more about commands not in this list.");
+    } else {
+        output(command + " - command cannot be processed");
+    }
+}
+
 function Command() {
     var command = commandTemplate.clone();
     var commandInput = command.find(".command-input");
@@ -30,6 +48,7 @@ function Command() {
         switch (e.key) {
             case 'Enter':
                 e.preventDefault();
+                executeCommand($(this).val());
                 new Command();
                 break;
         }
