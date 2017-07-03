1cc1162527153c02224595cd2c9b62e81aa12af0
dwijpr <dwijpr@gmail.com>
2017-07-03 16:03:29 +0700

    cv header

--- resources/views/cv/header.blade.php
+++ resources/views/cv/header.blade.php
@@ -27,7 +27,6 @@
     left: 160px;
 ">
     <div style="
-        /*border: 1px solid red;*/
         width: 370px;
         height: 130px;
         padding-left: 160px;
@@ -48,171 +47,24 @@
     height: 242px;
     font-size: 70%;
     padding: 4px;
+    text-align: right;
 ">
-    <h3 style="margin: 0;text-align: center;color: #89f;">
-        Full-Stack Web Developer
-    </h3>
-    <div style="
-        position: relative;
-        padding-top: 12px;
-    ">
-        <div style="
-            position: absolute;
-            right: 50%;
-            padding-right: 10px;
-            opacity: .5;
-        ">
-            FrontEnd
-        </div>
-        <div style="
-            position: absolute;
-            top: 28px;
-            left: 62px;
-            opacity: .5;
-        ">
-            JavaScript
-        </div>
-        <div style="
-            position: absolute;
-            top: 46px;
-            left: 78px;
-            opacity: .5;
-        ">
-            CSS
-        </div>
-        <div style="
-            position: absolute;
-            top: 32px;
-            left: 140px;
-            opacity: .5;
-        ">
-            HTML
-        </div>
-        <div style="
-            position: absolute;
-            top: 49px;
-            left: 112px;
-            opacity: .5;
-        ">
-            <small><i>get</i></small>Bootstrap
-        </div>
-        <div style="
-            position: absolute;
-            top: 54px;
-            left: 194px;
-            opacity: .5;
-        ">
-            JQuery
-        </div>
-        <div style="
-            position: absolute;
-            top: 16px;
-            left: 50%;
-            padding-left: 10px;
-            opacity: .5;
-        ">
-            BackEnd
-        </div>
-        <div style="
-            position: absolute;
-            top: 18px;
-            right: 55px;
-            color: #F36662;
-        ">
-            <b>Laravel</b>
-        </div>
-        <div style="
-            position: absolute;
-            top: 35px;
-            right: 72px;
-            opacity: .5;
-        ">
-            PHP
-        </div>
-        <div style="
-            position: absolute;
-            top: 36px;
-            right: 103px;
-            opacity: .5;
-        ">
-            CodeIgniter
-        </div>
-        <div style="
-            position: absolute;
-            top: 53px;
-            right: 67px;
-            opacity: .5;
-        ">
-            Web API
-        </div>
-        <div style="
-            position: absolute;
-            top: 71px;
-            left: 96px;
-            opacity: .5;
-        ">
-            <span>
-                Git
-            </span>
-        </div>
-        <div style="
-            position: absolute;
-            top: 69px;
-            left: 23px;
-            opacity: .5;
-        ">
-            TeamCity
-        </div>
-        <div style="
-            position: absolute;
-            top: 88px;
-            right: 103px;
-            opacity: .5;
-        ">
-            GitLab
-        </div>
-        <div style="
-            position: absolute;
-            top: 72px;
-            right: 23px;
-            opacity: .5;
-        ">
-            Continuous [Integration/Delivery]
-        </div>
-        <div style="
-            position: absolute;
-            top: 89px;
-            left: 108px;
-            opacity: .5;
-        ">
-            CLI
-        </div>
-        <div style="
-            position: absolute;
-            top: 88px;
-            left: 138px;
-            opacity: .5;
-        ">
-            Ba<i style="color: #0f0;">sh</i>
-        </div>
-        <div style="
-            position: absolute;
-            top: 89px;
-            left: 178px;
-            opacity: .5;
-        ">
-            AWS
-        </div>
-        <div style="
-            width: 100%;
-            text-align: center;
-            position: absolute;
-            top: 108px;
-            opacity: .5;
-        ">
-            <span>
-            Ubuntu Server
-            </span>
-        </div>
+    <h3>Full-Stack Web Laravel PHP Developer</h3>
+    <div class="skill">
+        <div>JavaScript</div>
+        <div>CSS</div>
+        <div>HTML</div>
+        <div><small><i>get</i></small>Bootstrap</div>
+        <div>JQuery</div>
+        <div>CodeIgniter</div>
+        <div>Web API</div>
+        <div>Git</div>
+        <div>TeamCity</div>
+        <div>GitLab</div>
+        <div>Continuous [Integration/Delivery]</div>
+        <div>CLI</div>
+        <div>Ba<i>sh</i></div>
+        <div>AWS</div>
+        <div><span>Ubuntu Server</span></div>
     </div>
 </div>
