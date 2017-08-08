6edd7459299f6bb3d74b5dbaeca22fa56e5aaa0d
dwijpr <dwijpr@gmail.com>
2017-08-09 06:23:20 +0700

    update topik 2

---
+++ resources/views/islam/arab/topik/2.blade.php
@@ -0,0 +1,93 @@
+@extends('layout.base')
+
+@section('style')
+    @parent
+    @include('islam.arab.style')
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-sm-12">
+            <h1>Topik 2: Nakiroh Ma'rifah</h1>
+
+            <p>
+                Bismillahirrahmanirrahim.
+                <br>
+                Pada topik 1: telah dibahas secara sepintas pembagian jenis kata
+                dalam bahasa Arab, dimana kata dalam bahasa Arab hanya dibagi 3,
+                yaitu kata benda (isim), kata kerja (fi'il), dan kata tugas (harf).
+                Contoh yang dipakai menggunakan surat Al-Baqaroh ayat 1-2. Pada
+                topik kali ini akan dibahas mengenai topik kata benda, mengenai
+                kata benda telah diketahui, dan kata benda belum diketahui.
+            </p>
+
+            <p>
+                Baiklah, kita mulai saja.
+            </p>
+
+            <p>
+                Pada topik 1, ada penanya (syukron atas pertanyaannya), yang
+                menanyakan kalau memang kata benda (isim) dalam bahasa Arab, sangat
+                tergantung pada jenis kelamin kata (apakah perempuan atau laki-laki),
+                maka bagaimana cara menentukan apakah kata benda ini berjenis
+                laki-laki (male), atau berjenis perempuan (female).
+            </p>
+
+            <p>
+                ambil contoh:
+                <br>
+                <span class="arab">ذلك كتاب</span>
+                <br>
+                dzalika kitaabun = itu (sebuah) kitab. Adalah struktur kalimat
+                yang benar. Karena kata dzalika (berjenis laki-laki), kitaabun
+                (berjenis laki-laki).
+            </p>
+
+            <p>
+                <span class="arab">ذلك شجرة</span>
+                <br>
+                dzalika syajaratun = itu (sebuah) pohon. Adalah struktur kalimat
+                yang salah. Karena kata dzalika (berjenis laki-laki), sedangkan
+                pohon (berjenis perempuan).
+            </p>
+
+            <p>
+                <span class="arab">تلك شجرة</span>
+                <br>
+                tilka syajaratun = itu (sebuah) pohon. Inilah kalimat yang benar,
+                karena dua-duanya merupakan kata benda berjenis perempuan.
+            </p>
+
+            <p>
+                Lalu bagaimana mengetahui suatu isim termasuk berjenis laki-laki
+                atau perempuan?
+            </p>
+
+            <p>
+                Untuk kata benda penunjuk (isim isyaroh) seperti "ini", "itu", maka
+                tidak ada cara kecuali dihapalkan saja. Wah repot dong.... Gak juga,
+                kata dalam kelompok ini tidak begitu banyak seperti:
+                <br>
+                <span class="arab">ذلك</span>
+                <br>
+                dzalika = itu (laki-laki)
+                <br>
+                <span class="arab">تلك</span>
+                <br>
+                tilka = itu (perempuan)
+                <br>
+                <span class="arab">هذا</span>
+                <br>
+                hadzaa = ini (laki - laki)
+                <br>
+                <span class="arab">هذه</span>
+                <br>
+                hadzihi = ini (perempuan)
+            </p>
+
+            <h2>Mengenalkan Teman</h2>
+        </div>
+    </div>
+</div>
+@endsection
