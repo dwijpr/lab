bdd800c295ae8bbf3fdca42d80c989d9118c49de
dwijpr <dwijpr@gmail.com>
2017-08-11 05:49:42 +0700

    add arabquran topik 3

---
+++ resources/views/islam/arab/topik/3.blade.php
@@ -0,0 +1,143 @@
+@extends('layout.base')
+
+@section('style')
+    @parent
+    @include('islam.arab.style')
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 col-sm-12">
+            <h1>Topik 3: Membentuk Kalimat Sempurna</h1>
+
+            <p>
+                Bismillahirrahmanirrahim.
+            </p>
+
+            <p>
+                Pada topik 2, kita telah menjelaskan apa bedanya kitaabun (buku)
+                dengan al-kitaabu (buku), yaitu perbedaannya terletak pada telah
+                jelas atau belum jelasnya benda yang dibicarakan oleh si pendengar.
+                Kebetulan tidak ada respon yang ditulis di Comments, maka saya
+                anggap, para pembaca telah mengerti. Tetapi ada yang bertemu dengan
+                saya dan menyampaikan kesimpulan dia di topik ke 2:
+            </p>
+
+            <p>
+                "Berarti yang akhirannya tun tun itu pasti jenis perempuan ya...?",
+                begitu kesimpulan teman saya tersebut. Saya belum jawab, karena
+                saya ingin share jawabannya di blog ini.
+            </p>
+
+            <p>
+                Oke sebelum masuk ke topik inti yaitu membentuk kalimat sempurna,
+                maka seperti biasa saya menjawab pertanyaan dulu. Oh ya, lain kali,
+                toloooongg bangeeet.... kalau ada pertanyaan tulisnya di Comments
+                (iconnya di akhir tulisan ini). Tujuannya adalah selain memudahkan saya
+                mencari jawabannya, pembaca lain juga jadi tahu apa yang dibahas.
+            </p>
+
+            <p>
+                Pertanyaan, atau lebih tepatnya pernyataan:
+                <br>
+                "Berarti yang akhirannya tun tun itu pasti jenis perempuan ya...?"
+                . Jawaban saya, iya kalau posisi dia sebagai mubtada' atau fa'il.
+                Nah mengenai mubtada' atau fa'il ini akan kita bahas pada topik-topik
+                berikutnya. Satu-satu ya teman, hehe, Belajar itu harus pelan-pelan,
+                biar meresap... Gitu kata ahli hikmah...
+            </p>
+
+            <p>
+                Kemaren saya ambil contoh: <span class="arab">شجرة</span>
+                syajaratun (pohon), ini kata berjenis perempuan. Lalu teman saya
+                mengatakan kalau begitu asal belakangnya tun tun pasti berjenis
+                perempuan. Seperti syajaratun, bintun (gadis perempuan) dsb.
+            </p>
+
+            <p>
+                Saya jawab iya. Tapi bagaimana kalau saya tulis begini
+                <span class="arab">شجرة</span> syajaratin, atau saya tulis
+                syajaratan. Saya memang tidak menuliskan harokat
+                <span class="arab"> ً ِ ُ ٌ </span> dan lain-lain, karena
+                kalau saya tuliskan, maka tanda-tanda titik seperti titik pada
+                "sya <span class="arab">ش</span>" akan tertimpa tanda harokat,
+                sehingga tidak jelas lagi (apakah ini keterbatasan Windows XP saya?)
+                Tapi gak apa-apa, karena dalam bahasa Arab yang asli, tanda-tanda
+                harokat juga tidak ada.
+            </p>
+
+            <p>
+                Kembali lagi, <span class="arab">شجرة</span> kalau pakai harokat
+                bisa saya tulis, syajaratun, syajaratan, syajaratin, syajaratu, syajarata,
+                dan syajarati, dan kalau dalam bahasa percakapan ta marbutoh nya
+                saya matikan/saya waqofkan (menjadi bunyi h), sehingga diucapkan
+                menjadi syajarah. Nah loh berarti ada 7 kemungkinan bacaan. Dimana
+                yang berakhiran tun merupakan salah satunya.
+            </p>
+
+            <p>
+                Lalu yang mana yang berjenis perempuan? Jawabannya ke 7 jenis bacaan itu,
+                tidak mempengaruhi status <span class="arab">شجرة</span> sebagai
+                kata benda berjenis perempuan. Apakah dibaca syajaratun, syajaratu,
+                syajaratan, syajarata, syajaratin, syajarati, maupun syajarah.
+            </p>
+
+            <p>
+                Lalu apa bedanya dong antara syajaratun dengan syajaratu. Hehe... ini
+                pertanyaan yang sama di kepala saya beberapa bulan yang lalu waktu
+                belajar bahasa Arab. Biar gak penasaran jawaban singkatnya, jika
+                ada AL (<span class="arab">الــ</span>) misal <span class="arab">الشجرة</span>
+                maka huruf n dibuang. Jadi <span class="arab">الشجرة</span> hanya boleh
+                dibaca as-syajaratu, as-syajarata, as-syajarati, atau as-syajarah.
+            </p>
+
+            <p>
+                Kok begitu? Ya begitulah hukum atau aturan-aturan dalam bahasa Arab,
+                yang hanya orang Arablah yang berhak membuat aturan-aturan itu.
+            </p>
+
+            <p>
+                Pertanyaan ke dua: kalau begitu, apa bedanya antara as-syajaratu
+                dengan as-syajarata? Jawaban ringkasnya adalah melihat kepada posisi
+                yang ditempati oleh kata tsb. Apakah dia sebagai subject atau sebagai
+                objek. Jika dia sebagai subject, maka dia dibaca as-syajaratu, jika
+                dia sebagai object maka dibaca as-syajarata.
+            </p>
+
+            <p>
+                Auuu...bingung... bisa kasih contoh gak?
+            </p>
+
+            <p>
+                Gini gini... Contoh:
+                <br>
+                Pohon in besar.
+                <span class="arab">هذه الشجرة كبيرة</span>
+                (hadzihi as-syajaratu kabiiratun)
+                <br>
+                Saya (telah) melihat (sebuah) pohon.
+                <span class="arab">رأيت الشجرة</span>
+                (ro-ai-tu as-syajarata)
+            </p>
+
+            <p>
+                Pohon dalam kalimat pertama berfungsi sebagai subject, maka dia
+                dibaca as-syajaratu, sedangkan Pohon dalam kalimat dua berfungsi
+                sebagai object maka dia dibaca as-syajarata.
+            </p>
+
+            <p>
+                Okeh... cukup dulu disitu jawab pertanyaannya ya... Sekarang ke
+                topik utama, membuat kalimat sempurna... Tapi pembaca... aduh...
+                udah kepanjangan... disambung nanti saja ya... Insya Allah...
+                jangan lupa isi comments dibawah ini. Satu lagi... Jangan kapok
+                ya belajar bahasa Arab... Insya Allah dapat pahala kok, karena
+                semakin ngerti bahasa Arab semakin asyik kan... Dengerin imam
+                yang suaranya tartil, merdu, merayu... bisa bisa nangis loh, karena
+                tau apa yang dibaca sang Imam... Syukron.
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
